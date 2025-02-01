ff66942412ec613aef63f7540695d2545e6ae0b6
mevlt01001 <mevlutbasaran01@gmail.com>
2025-02-01 20:00:37 +0300

    it is ready for first test

--- box.py
+++ box.py
@@ -9,23 +9,26 @@ class Box:
         self.class_id = class_id
         self.matched_box: Optional[Box] = None
 
-
-
 class PredBox(Box):
-    def __init__(self, x1, y1, x2, y2, class_id):
+    def __init__(self, x1, y1, x2, y2, class_id, conf):
         super().__init__(x1, y1, x2, y2, class_id)
-        self.iou=0
-
+        self.conf = conf
+        self.iou_val = 0
 
 class TruthBox(Box):
     def __init__(self, x1, y1, x2, y2, class_id):
         super().__init__(x1, y1, x2, y2, class_id)
 
     def iou(self, pred_box: PredBox) -> float:
-        intersect = (min(self.x2, pred_box.x2) - max(self.x1, pred_box.x1)) * (min(self.y2, pred_box.y2) - max(self.y1, pred_box.y1))
-        if intersect <= 0:
+        intersect_width = max(0, min(self.x2, pred_box.x2) - max(self.x1, pred_box.x1))
+        intersect_height = max(0, min(self.y2, pred_box.y2) - max(self.y1, pred_box.y1))
+        if intersect_width == 0 or intersect_height == 0:
             return 0
-        union = (self.x2 - self.x1) * (self.y2 - self.y1) + (pred_box.x2 - pred_box.x1) * (pred_box.y2 - pred_box.y1) - intersect
-        return intersect / union
+        intersect = intersect_width * intersect_height
+        truth_area = (self.x2 - self.x1) * (self.y2 - self.y1)
+        pred_area = (pred_box.x2 - pred_box.x1) * (pred_box.y2 - pred_box.y1)
+        union = truth_area + pred_area - intersect
+        iou = intersect / union        
+        return iou
 
         
--- image.py
+++ image.py
@@ -1,31 +1,36 @@
 from box import TruthBox, PredBox
-from confusion_matrix import ConfusionMatrix
 from typing import Optional
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
 
 class Image:
     def __init__(self):
-        self.truth_boxes: Optional[list[TruthBox]] = None
-        self.pred_boxes: Optional[list[PredBox]] = None
-        self.confusion_matrix = ConfusionMatrix()
-        
+        self.truth_boxes: Optional[list[TruthBox]] = []
+        self.pred_boxes: Optional[list[PredBox]] = []
+        self.CM_data: list[list[int]] = [] # confidence_score, TP, FP, FN
 
-    def load_truth_boxes(self, truth_boxes: list[TruthBox]):
-        self.truth_boxes = truth_boxes
+    def load_truth_boxes(self, truth_boxes: list):
+        for truth_box in truth_boxes:
+            new_truth_box = TruthBox(truth_box[0], truth_box[1], truth_box[2], truth_box[3], truth_box[4])
+            self.truth_boxes.append(new_truth_box)
     
-    def load_pred_boxes(self, pred_boxes: list[PredBox]):
-        self.pred_boxes = pred_boxes
+    def load_pred_boxes(self, pred_boxes: list):
+        for pred_box in pred_boxes:
+            new_pred_box = PredBox(pred_box[0], pred_box[1], pred_box[2], pred_box[3], pred_box[4], pred_box[5])
+            self.pred_boxes.append(new_pred_box)
 
-    def load_boxes(self, truth_boxes: list[TruthBox], pred_boxes: list[PredBox]):
-        self.truth_boxes = truth_boxes
-        self.pred_boxes = pred_boxes
+    def load_boxes(self, truth_boxes: list, pred_boxes: list, iou_treshold = 0.5):
+        self.load_truth_boxes(truth_boxes)
+        self.load_pred_boxes(pred_boxes)
+        self.compute_confusion_matrix(iou=iou_treshold)
 
     def match_boxes(self, iou: float):
         for truth_box in self.truth_boxes:
-            
             if truth_box.matched_box is not None:
                 continue
-            
-            best_pred_box: PredBox = None
+
+            best_pred_box = None
             best_iou = -1
 
             for pred_box in self.pred_boxes:
@@ -33,37 +38,86 @@ class Image:
                     continue
 
                 current_iou = truth_box.iou(pred_box)
-                if current_iou > best_iou:
+                if current_iou > best_iou and pred_box.class_id == truth_box.class_id:
                     best_iou = current_iou
                     best_pred_box = pred_box
-            
-            if best_pred_box is not None and best_iou > iou:
+                    
+
+            if best_pred_box is not None and best_iou >= iou:
                 truth_box.matched_box = best_pred_box
                 best_pred_box.matched_box = truth_box
-                best_pred_box.iou = best_iou                
-
+                best_pred_box.iou_val = best_iou
 
 
     def compute_confusion_matrix(self, iou: float):
         self.match_boxes(iou)
 
-        # TP = truth_box.matched_box is not None and truth_box.class_id == truth_box.matched_box.class_id | A truth box has matched a pred box that has same class id with it
-        # TN = İgnore
-        # FP = pred_box.matched_box == None or pred_box.matched_box.class_id != pred_box.class_id | A pred box hasn't matched any truth box or it has matched a truth box that hasn't same class id with it
-        # FN = truth_box.matched_box == None
-
         for truth_box in self.truth_boxes: 
-            if truth_box.matched_box is None:# if a truth box hasn't matched a predicted box
-                self.confusion_matrix.FN += 1
+            if truth_box.matched_box is None or (truth_box.matched_box is not None and truth_box.matched_box.class_id != truth_box.class_id):# if a truth box hasn't matched a predicted box
+                self.CM_data.append([0, 0, 0, 1])
             else:# if a truth box has matched a predicted box
                 if truth_box.class_id == truth_box.matched_box.class_id: # if a truth box has matched a predicted box and it's class id has same value with it's predicted box's class id
-                    self.confusion_matrix.TP += 1
-                else:# if a truth box has matched a predicted box and it's class id has not same value with it's predicted box's class id
-                    self.confusion_matrix.FP += 1
+                    self.CM_data.append([truth_box.matched_box.conf, 1, 0, 0])
 
         for pred_box in self.pred_boxes:
             if pred_box.matched_box is None:# if a pred box hasn't match a truth box
-                self.confusion_matrix.FP += 1
+                self.CM_data.append([pred_box.conf, 0, 1, 0])
+
+    def get_cm(self):
+        return self.CM_data
 
 
+class Frames:
+    def __init__(self, whole_truth_boxes, whole_pred_boxes):
+        self.whole_truth_boxes = whole_truth_boxes
+        self.whole_pred_boxes = whole_pred_boxes
+        self.images: list[Image] = []
+        self.data = []
 
+    def load_images(self, iou_treshold = 0.5):
+        for i in range(len(self.whole_truth_boxes)):
+            image = Image()
+            image.load_boxes(self.whole_truth_boxes[i], self.whole_pred_boxes[i], iou_treshold=iou_treshold)
+            self.images.append(image)
+            self.data.extend(image.get_cm())#conf, TP, FP, FN
+
+        self.data = np.array(self.data)
+        self.data = self.data[np.argsort(self.data[:, 0])[::-1]]
+
+        cum_data = self.data[:, 1:].cumsum(axis=0)#cum_TP, cum_FP, cum_FN
+        precision = self.calc_precision(cum_data[:, 0], cum_data[:, 1]).reshape(-1, 1)
+        recall = self.calc_recall(cum_data[:, 0], cum_data[:, 2]).reshape(-1, 1)
+        f1_score = self.calc_f1_score(precision, recall).reshape(-1, 1)
+
+        print(f"data: {self.data.shape}\ncum_data : {cum_data.shape}\nprecision: {precision.shape}\nrecall: {recall.shape}\nf1_score: {f1_score.shape}")
+
+        self.data = np.concatenate((self.data, cum_data, precision, recall, f1_score), axis=1)
+
+        print(f"data: {self.data.shape}")
+        self.data = pd.DataFrame(self.data, columns=['conf', 'TP', 'FP', 'FN', 'cum_TP', 'cum_FP', 'cum_FN', 'precision', 'recall', 'f1_score'])        
+
+    def calc_precision(self, TP, FP):
+        return TP / (TP + FP)
+
+    def calc_recall(self, TP, FN):
+        return TP / (TP + FN)
+
+    def calc_f1_score(self, precision, recall):
+        return 2 * (precision * recall) / (precision + recall)
+    
+    def plot_ap(self):
+        recall = self.data['recall']
+        precision = self.data['precision']
+
+        plt.plot(recall, precision, color='b', label=f'AP: {self.calc_mAP()}')
+        plt.xlabel('Recall')
+        plt.ylabel('Precision')
+        plt.title('Precision-Recall Curve')
+        plt.legend()
+        plt.show()
+    
+    def calc_mAP(self):
+        recall = self.data['recall']
+        precision = self.data['precision']
+
+        return np.trapz(y=precision, x=recall, dx=0.0001)
